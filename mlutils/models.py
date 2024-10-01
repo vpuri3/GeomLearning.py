found bug in edge_index calculation

mlutils/models.py
@@ -3,7 +3,7 @@ import torch
 from torch import nn
 
 import torch_scatter
-import torch_geometric
+import torch_geometric as pyg
 from torch_geometric.nn import MessagePassing
 
 import math
@@ -164,22 +164,43 @@ def double_conv(ci, co, k):
     )
 
 class UNet(nn.Module):
-    def __init__(self, ci, co, k):
+    def __init__(self, ci, co, k, w=256, H=None, W=None):
         super().__init__()
 
+        ######################
         self.dconv_down1 = double_conv(ci ,  64, k)
         self.dconv_down2 = double_conv(64 , 128, k)
         self.dconv_down3 = double_conv(128, 256, k)
         self.dconv_down4 = double_conv(256, 512, k)
-
+        
         self.maxpool = nn.MaxPool2d(2)
         self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-
+        
         self.dconv_up3 = double_conv(256 + 512, 256, k)
         self.dconv_up2 = double_conv(128 + 256, 128, k)
         self.dconv_up1 = double_conv(128 +  64,  64, k)
-
+        
         self.conv_last = nn.Conv2d(64, co, 1)
+
+        # self.ln_down1 = nn.LayerNorm([ 64, H//2, W//2])
+        # self.ln_down2 = nn.LayerNorm([128, H//2, W//2])
+        # self.ln_down3 = nn.LayerNorm([256, H//2, W//2])
+        # self.ln_down4 = nn.LayerNorm([512, H//2, W//2])
+        ######################
+
+        # self.dconv_down1 = double_conv(ci, w, k)
+        # self.dconv_down2 = double_conv( w, w, k)
+        # self.dconv_down3 = double_conv( w, w, k)
+        # self.dconv_down4 = double_conv( w, w, k)
+        #
+        # self.maxpool = nn.MaxPool2d(2)
+        # self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        #
+        # self.dconv_up3 = double_conv(2*w, w, k)
+        # self.dconv_up2 = double_conv(2*w, w, k)
+        # self.dconv_up1 = double_conv(2*w, w, k)
+        #
+        # self.conv_last = nn.Conv2d(w, co, 1)
 
     def forward(self, x):
         conv1 = self.dconv_down1(x)

mlutils/train.py
@@ -2,8 +2,7 @@
 # 3rd party
 import torch
 from torch import nn, optim
-from torch.optim import lr_scheduler
-from torch.utils.data import DataLoader
+import torch_geometric as pyg
 
 from tqdm import tqdm
 
@@ -26,6 +25,7 @@ class Trainer:
         _data,
         data_=None,
 
+        gnn=False,
         device=None,
 
         _batch_size=None,
@@ -48,55 +48,16 @@ class Trainer:
     ):
 
         # TODO: early stopping
-        # TODO: loader pin_memory=True, pin_memory_device=device
-        # would then remove batch.to(device) calls in training loop
-        # TODO: loader sampler (replacement = True)
-        # check out minigpt for reference on adding callbacks
-
-        device = select_device(device, verbose=True)
+        device = select_device(device, verbose=verbose)
 
         # DATA
         if _batch_size is None:
             _batch_size = 32
         if __batch_size is None:
             __batch_size = len(_data)
-        #
-
-        # branch on isinstance(_data, torch.utils.data.Dataset)
-
-        # if isinstance(_data[0], torch_geometric.data.Data):
-        #     pass
-        # else: # use torch
-        #     pass
-
-        # train_loader = DataLoader(
-        #     self.train_dataset,
-        #     sampler=torch.utils.data.RandomSampler(self.train_dataset, replacement=True, num_samples=int(1e10)),
-        #     shuffle=False,
-        #     pin_memory=True,
-        #     batch_size=config.batch_size,
-        #     num_workers=config.num_workers,
-        # )
-
-        _loader  = DataLoader(_data, batch_size=_batch_size)
-        __loader = DataLoader(_data, batch_size=__batch_size, shuffle=False)
-
         if data_ is not None:
-            if verbose:
-                print(f"Number of training samples: {len(_data)}")
             if batch_size_ is None:
                 batch_size_ = len(data_)
-            loader_ = DataLoader(data_, batch_size=batch_size_ , shuffle=False)
-        else:
-            if verbose:
-                print(f"Number of test samples: 0")
-            loader_ = None
-
-        if verbose:
-            for (x, y) in _loader:
-                print(f"Shape of x: {x.shape} {x.dtype}")
-                print(f"Shape of u: {y.shape} {y.dtype}")
-                break
 
         # MODEL
         if verbose:
@@ -127,21 +88,22 @@ class Trainer:
 
         if Schedule == "OneCycleLR":
             niters = nepochs * len(_loader)
-            schedule = lr_scheduler.OneCycleLR(opt, 1e-2, total_steps=niters)
+            schedule = optim.lr_scheduler.OneCycleLR(opt, 1e-2, total_steps=niters)
         elif Schedule == "CosineAnnealingLR":
             niters = nepochs * len(_loader)
-            schedule = lr_scheduler.CosineAnnealingLR(opt, niters)
+            schedule = optim.lr_scheduler.CosineAnnealingLR(opt, niters)
         elif Schedule is None:
-            schedule = lr_scheduler.ConstantLR(opt, factor=1.0, total_iters=1e10)
+            schedule = optim.lr_scheduler.ConstantLR(opt, factor=1.0, total_iters=1e10)
         else:
             raise NotImplementedError()
 
         config = {
+            "gnn" : gnn,
             "device" : device,
 
-            "data_size" : len(_loader.dataset),
-            "num_batches" : len(_loader),
-            "batch_size" : _loader.batch_size,
+            "data_size" : len(_data),
+            "num_batches" : len(_data) // _batch_size,
+            "batch_size" : _batch_size,
 
             "num_parameters" : num_parameters(model),
 
@@ -163,20 +125,28 @@ class Trainer:
             print(f"{k} : {v}")
 
         # ASSIGN TO SELF
+
+        # MISC
+        self.gnn = gnn
         self.device = device
+        self.verbose = verbose
 
-        self._loader  = _loader
-        self.__loader = __loader
-        self.loader_  = loader_
+        # DATA
+        self._data = _data
+        self.data_ = data_
+        self._batch_size = _batch_size
+        self.batch_size_ = batch_size_
+        self.__batch_size = __batch_size
 
+        # MODEL
         self.model = model
 
+        # OPT
         self.opt = opt
         self.schedule = schedule
 
         self.lossfun = lossfun
         self.nepochs = nepochs
-
         self.statsfun = statsfun
 
         self.wandb = wandb
@@ -219,15 +189,62 @@ class Trainer:
         self.model.load_state_dict(data["model"])
         self.model.to(self.device)
 
+    def make_dataloader(self):
+        # TODO: loader pin_memory=True, pin_memory_device=device
+        # would then remove batch.to(device) calls in training loop
+        # TODO: loader sampler (replacement = True)
+
+        if self.gnn:
+            DL = pyg.data.DataLoader
+        else:
+            DL = torch.utils.data.DataLoader
+
+        self._loader  = DL(self._data, batch_size=self._batch_size, shuffle=True)
+        self.__loader = DL(self._data, batch_size=self.__batch_size, shuffle=False)
+        if self.data_ is not None:
+            self.loader_ = DL(self.data_, batch_size=self.batch_size_ , shuffle=False)
+        else:
+            self.loader_ = None
+
+        if self.verbose:
+            print(f"Number of training samples: {len(self._data)}")
+            if self.data_ is not None:
+                print(f"Number of test samples: {len(self.data_)}")
+            else:
+                print(f"No test data provided")
+
+        if self.verbose:
+            if self.gnn:
+                for batch in self._loader:
+                    print(batch)
+                    break
+            else:
+                for (x, y) in self._loader:
+                    print(f"Shape of x: {x.shape} {x.dtype}")
+                    print(f"Shape of u: {y.shape} {y.dtype}")
+                    break
+
+        # _loader = DL(
+        #     self.train_dataset,
+        #     sampler=torch.utils.data.RandomSampler(self.train_dataset, replacement=True, num_samples=int(1e10)),
+        #     shuffle=False,
+        #     pin_memory=True,
+        #     batch_size=config.batch_size,
+        #     num_workers=config.num_workers,
+        # )
+
+        return
+
     def train(self):
         # move to device here?
+        self.make_dataloader()
 
         self.start_time = time.time()
         self.print_train_banner(self.epoch, self.nepochs)
         self.trigger_callback("epoch_end")
         self.statistics()
 
-        for _ in range(1, self.nepochs + 1):
+        while self.epoch < self.nepochs:
             self.epoch += 1
             self.print_train_banner(self.epoch, self.nepochs)
 
@@ -249,12 +266,8 @@ class Trainer:
             bar_format='{n_fmt}/{total_fmt} {desc}{bar}[{rate_fmt}]',
         )
 
-        for (x, y) in batch_iterator:
-            x, y = x.to(self.device), y.to(self.device)
-
-            yh = self.model(x)
-            loss = self.lossfun(yh, y)
-
+        for batch in batch_iterator:
+            loss = self.batch_loss(batch)
             loss.backward()
             self.opt.step()
             self.schedule.step()
@@ -266,8 +279,21 @@ class Trainer:
             )
             self.trigger_callback("batch_end")
         #
-
         return
+
+    def batch_loss(self, batch):
+        if self.gnn:
+            batch = batch.to(self.device)
+            yh = self.model(batch)
+            loss = self.lossfun(yh, batch.y)
+        else:
+            x = batch[0].to(self.device)
+            y = batch[1].to(self.device)
+
+            yh = self.model(x)
+            loss = self.lossfun(yh, y)
+        #
+        return loss
 
     @torch.no_grad()
     def evaluate(self, loader):
@@ -279,16 +305,12 @@ class Trainer:
         # if statsfun:
         #     pass
 
-        for (x, y) in loader:
-            x, y = x.to(self.device), y.to(self.device)
-            yh = self.model(x)
-            loss = self.lossfun(yh, y)
+        for batch in loader:
+            loss = self.batch_loss(batch)
             avg_loss += loss.item()
 
             # if statsfun:
             #     pass
-            #
-            #
         #
 
         nbatches = len(loader)

mlutils/utils.py
@@ -45,13 +45,13 @@ def mean_std(x: torch.tensor, channel_dim = None):
         channel_dim = x.ndim-1
     del dims[channel_dim]
 
-    xbar = x.mean(dims)
-    xstd = x.std(dims)
+    xbar = x.mean(dims, keepdim=True)
+    xstd = x.std(dims, keepdim=True)
 
-    # fix broadcasting
-    for _ in range(x.ndim-channel_dim-1):
-        xbar = xbar.unsqueeze(-1)
-        xstd = xstd.unsqueeze(-1)
+    # # fix broadcasting
+    # for _ in range(x.ndim-channel_dim-1):
+    #     xbar = xbar.unsqueeze(-1)
+    #     xstd = xstd.unsqueeze(-1)
 
     return xbar, xstd
 

sandbox/__main__.py
@@ -2,7 +2,7 @@
 # 3rd party
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
+import torch_geometric as pyg
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,11 +15,12 @@ import argparse
 import sandbox
 import mlutils
 
-def train_loop(model, data, lrs=None, nepochs=None, **kw):
+def train_loop(model, data, E=100, lrs=None, nepochs=None, **kw):
     if lrs is None:
         lrs = [5e-4, 1e-4, 5e-5, 1e-5, 5e-6]
     if nepochs is None:
-        nepochs = [10, 20, 20, 10, 5]
+        nepochs = [.20*E, .25*E, .25*E, .20*E, .1*E]
+        nepochs = [int(e) for e in nepochs]
     assert len(lrs) == len(nepochs)
 
     for i in range(len(lrs)):
@@ -36,29 +37,58 @@ def train_loop(model, data, lrs=None, nepochs=None, **kw):
 class MaskedUNet(nn.Module):
     def __init__(self, shape, ci, co, k):
         super().__init__()
-
         self.shape = shape
         self.unet = mlutils.UNet(ci, co, k)
-
-        # M = shape.final_mask().unsqueeze(0).unsqueeze(0)
-        # self.register_buffer('M', M)
 
     @torch.no_grad()
     def compute_mask(self, x):
         x0, z0, t0 = [x[0,c,:,:] for c in range(3)]
         t1 = t0 + self.shape.dt()
         M = self.shape.mask(x0, z0, t1)
-
-        # M = self.shape.mask(x0, z0, torch.ones_like(t0))
-
         return M.unsqueeze(0).unsqueeze(0)
 
     def forward(self, x):
-        y = self.unet(x)
         M = self.compute_mask(x)
-
-        return y * M
+        x = self.unet(x)
+        return x * M
 #
+
+class MaskedUNet2(nn.Module):
+    def __init__(self, shape, w=128, act=nn.ReLU()):
+        super().__init__()
+        self.encoder = nn.Sequential(                 # [N, 3, 256, 256]
+            mlutils.C2d_block(4, w, None, "2x", act), # [128]
+            mlutils.C2d_block(w, w, None, "2x", act), # [64]
+            mlutils.C2d_block(w, w, None, "2x", act), # [32]
+            mlutils.C2d_block(w, w, None, "2x", act), # [16]
+        )
+
+        self.bottleneck = nn.Sequential( # [N, w, 16, 16]
+            nn.Conv2d(w, w, kernel_size=3, padding=1), act, # [16]
+            nn.Conv2d(w, w, kernel_size=3, padding=1), act, # [16]
+        )
+
+        self.decoder = nn.Sequential( # [N, w, 16, 16]
+            mlutils.CT2d_block(w, w, None, "2x", act), # 32
+            mlutils.CT2d_block(w, w, None, "2x", act), # 64
+            mlutils.CT2d_block(w, w, None, "2x", act), # 128
+            mlutils.CT2d_block(w, 1, None, "2x", act), # 256
+        )
+        return
+
+    @torch.no_grad()
+    def compute_mask(self, x):
+        x0, z0, t0 = [x[0,c,:,:] for c in range(3)]
+        t1 = t0 + self.shape.dt()
+        M = self.shape.mask(x0, z0, t1)
+        return M.unsqueeze(0).unsqueeze(0)
+
+    def forward(self, x):
+        M = self.compute_mask(x)
+        x = self.encoder(x)
+        x = self.bottleneck(x)
+        x = self.decoder(x)
+        return x * M
 
 def train_cnn(device, outdir, resdir, name, blend=True, train=True):
 
@@ -93,7 +123,7 @@ def train_cnn(device, outdir, resdir, name, blend=True, train=True):
 
     # TRAIN
     if train:
-        train_loop(model, data, device=device, _batch_size=2)
+        train_loop(model, data, device=device, _batch_size=4, E=100)
         torch.save(model.to("cpu").state_dict(), modelfile)
 
     # VISUALIZE
@@ -140,6 +170,37 @@ def train_cnn(device, outdir, resdir, name, blend=True, train=True):
         fig2.savefig(imagefile2, dpi=300)
     return
 
+class MaskedGNN(nn.Module):
+    def __init__(self, shape, ci, co, w):
+        super().__init__()
+        self.shape = shape
+        # self.gnn = pyg.nn.Sequential("x, edge_index", [
+        #     (pyg.nn.GCNConv(ci, w), "x, edge_index -> x"),
+        #     nn.ReLU(),
+        #     (pyg.nn.GCNConv(w, co), "x, edge_index -> x"),
+        # ])
+        self.conv1 = pyg.nn.GCNConv(ci,  w)
+        self.conv2 = pyg.nn.GCNConv( w, co)
+
+    @torch.no_grad()
+    def compute_mask(self, x):
+        x0, z0, t0 = [x[0,c,:,:] for c in range(3)]
+        t1 = t0 + self.shape.dt()
+        M = self.shape.mask(x0, z0, t1)
+        return M.unsqueeze(0).unsqueeze(0)
+
+    def forward(self, data):
+        # M = self.compute_mask(x)
+        # x = self.gnn(x)
+        # return x * M
+        # return self.gnn(data.x, data.edge_index)
+        x, edge_index = data.x, data.edge_index
+        x = self.conv1(x, edge_index)
+        x = nn.functional.relu(x)
+        x = self.conv2(x, edge_index)
+        return x
+#
+
 def train_gnn(device, outdir, resdir, name, blend=True, train=True):
 
     outname = outdir + "gnn_nextstep_" + name
@@ -169,13 +230,16 @@ def train_gnn(device, outdir, resdir, name, blend=True, train=True):
 
     # nx, nz, nt = 128, 128, 100
     # shape = sandbox.Shape(nx, nz, nt, nw1, nw2)
-    data = sandbox.makedata(
+    data, metadata = sandbox.makedata(
         shape, inputs="tT", outputs="T", datatype="graph", mask="finaltime",
     )
 
-    # # MODEL
-    # ci, co, k = 2, 1, 3
-    # model = MaskedUNet(shape, ci, co, k)
+    # MODEL
+    ci, co, w = 2, 1, 32
+    model = MaskedGNN(shape, ci, co, w)
+    if train:
+        train_loop(model, data, device=device, E=50, gnn=True, _batch_size=4)
+        torch.save(model.to("cpu").state_dict(), modelfile)
 
     return
 
@@ -237,8 +301,10 @@ if __name__ == "__main__":
     resdir = "./res/"
 
     # view_shape(resdir, "alldomain")
-    # view_shape(resdir, "hourglass")
+    view_shape(resdir, "hourglass")
 
     # train_cnn(device, outdir, resdir, "alldomain", train=True)
-    train_cnn(device, outdir, resdir, "hourglass", train=False)
+    # train_cnn(device, outdir, resdir, "hourglass", train=True)
+
+    # train_gnn(device, outdir, resdir, "hourglass", train=True)
 #

sandbox/dataset.py
@@ -1,5 +1,6 @@
 #
 import torch
+import torch_geometric as pyg
 from torch.utils.data import Dataset, TensorDataset
 
 import mlutils
@@ -88,31 +89,37 @@ def makedata(
         value = (value[1:] - value[0:-1]) / shape.dt()
         channel_dim = 1
     elif datatype == "graph":
-
         assert mask == "finaltime"
 
-        point = torch.stack(point, dim=point[0].ndim) # [Nt, Nz, Nx, C]
-        value = torch.stack(value, dim=value[0].ndim)
+        point = torch.stack(point, dim=-1) # [Nt, Nz, Nx, C]
+        value = torch.stack(value, dim=-1)
 
-        edges, idx_lin, idx_cart = shape.create_final_graph()
+        edge_index, idx_lin, idx_cart = shape.create_final_graph()
         iz, ix =  idx_cart[:, 0], idx_cart[:, 1]
 
         # node features # (Temp, time)
-        point = point[:, ix, iz, :] # [Nt, V, C]
+        point = point[:, ix, iz, :] # [Nt, Nodes, C]
         value = value[:, ix, iz, :]
 
         # edge features # (x-relative, z-relative)
-        x = x.reshape(self.nt, -1)
-        z = z.reshape(self.nt, -1)
+        x = x.reshape(shape.nt, -1)
+        z = z.reshape(shape.nt, -1)
 
-        edge_x = x[:, edges[:, 0]] - x[:, edges[1]]
-        edge_z = z[:, edges[:, 0]] - z[:, edges[1]]
+        edge_x = x[:, edge_index[0]] - x[:, edge_index[1]]
+        edge_z = z[:, edge_index[0]] - z[:, edge_index[1]]
+        edge_attr = torch.stack([edge_x, edge_z], dim=-1) # [Nt, Nedges, C]
 
-        edge_features = torch.stack([edge_x, edge_z], dim=1)
+        ## edge_index has the global indices, not indices corresopnding to
+        ## the active graph
 
-        channel_dim = 4
+        print(edge_index.shape)
+        print(torch.max(edge_index[0]))
+        print(torch.max(edge_index[1]))
+        print(point.shape)
+        print(value.shape)
+        assert False
 
-        raise NotImplementedError(f"Mesh datatype not implemented.")
+        channel_dim = 2
     else:
         raise NotImplementedError(f"Got incompatible datatype: {datatype}.")
 
@@ -120,6 +127,10 @@ def makedata(
     ybar, ystd = mlutils.mean_std(value, channel_dim)
 
     metadata = {
+        # shape
+        "nx" : shape.nx, "nz" : shape.nz, "nt" : shape.nt,
+        "nw1" : shape.nw1, "nw2" : shape.nw2,
+        # data
         "channel_dim" : channel_dim,
         "xbar" : xbar, "xstd" : xstd,
         "ybar" : ybar, "ystd" : ystd,
@@ -128,7 +139,18 @@ def makedata(
     # point = mlutils.normalize(point, xbar, xstd)
     # value = mlutils.normalize(value, ybar, ystd)
 
-    data = TensorDataset(point, value)
+    if "graph" in datatype:
+        data = [
+            pyg.data.Data(x=point[i], y=value[i],
+                edge_index=edge_index, edge_aatr=edge_attr,
+            )
+            for i in range(shape.nt)
+        ]
+        metadata = {
+            **metadata, "idx_lin" : idx_lin, "idx_cart" : idx_cart,
+        }
+    else:
+        data = TensorDataset(point, value)
 
     if split is None:
         return data, metadata

sandbox/shape.py
@@ -122,7 +122,7 @@ class Shape:
         iz = idx // self.nx
         return (ix, iz)
 
-    def compute_final_graph(self, bidirectional=True, debug=False):
+    def create_final_graph(self, bidirectional=True, debug=False):
         M = self.final_mask()
 
         idx_cart = torch.argwhere(M)
@@ -175,21 +175,24 @@ class Shape:
             # prune between left/right, up/down
             raise NotImplementedError()
 
-        edges = torch.cat([
+        edge_index = torch.cat([
             lft_edges,
             rgt_edges,
             top_edges,
             btm_edges,
         ], dim=0)
 
-        return edges, idx_lin, idx_cart
+        # edge_index [Nedges, 2]
+        edge_index = edge_index.T.contiguous()
+
+        return edge_index, idx_lin, idx_cart
 
     @torch.no_grad()
     def plot_final_graph(self, debug=False):
-        edges, _, idx_cart = self.compute_final_graph(debug=debug)
+        edge_index, _, idx_cart = self.create_final_graph(debug=debug)
 
-        edges    = edges.numpy(force=True)
-        idx_cart = idx_cart.numpy(force=True)
+        edge_index = edge_index.numpy(force=True)
+        idx_cart   = idx_cart.numpy(force=True)
 
         # background graph
         bg_graph = nx.Graph()
@@ -202,11 +205,11 @@ class Shape:
         bg_graph.add_edges_from(bg_edgeV)
 
         # active graph
+        _ix, _iz = self.cartesian_index(edge_index[0])
+        ix_, iz_ = self.cartesian_index(edge_index[1])
+
         graph = nx.DiGraph()
         nodes = [(idx[1], idx[0]) for idx in idx_cart]
-        _ix, _iz = self.cartesian_index(edges[:, 0])
-        ix_, iz_ = self.cartesian_index(edges[:, 1])
-
         edges = [((_ix[i], _iz[i]), (ix_[i], iz_[i])) for i in range(len(_ix))]
 
         graph.add_nodes_from(nodes)