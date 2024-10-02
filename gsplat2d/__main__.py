988ce98badd648411e2b8be1bfb55fcb16aa53f6
Vedant Puri <vedantpuri@gmail.com>
2024-10-02 09:33:28 -0400

    gaussian splatting intro

---
+++ gsplat2d/__main__.py
@@ -0,0 +1,158 @@
+#
+# https://github.com/OutofAi/2D-Gaussian-Splatting/
+#
+import torch
+from torch import nn, optim
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL
+
+# local
+import gsplat2d
+import mlutils
+
+# builtin
+import os
+import argparse
+import requests
+
+def test_raster(device, outdir, resdir):
+
+    # scale_x = torch.tensor([1.0, 0.5, 0.5], device=device)
+    # scale_y = torch.tensor([1.0, 0.5, 1.5], device=device)
+    # scale = torch.stack([scale_x, scale_y], dim=-1)
+    # rotation = torch.tensor([0.0, 0.0, -0.5], device=device) # a value between -pi/2 and pi/2
+    # means = torch.tensor([(-0.5, -0.5), (0.8, 0.8), (0.5, 0.5)], device=device)
+    # colors  = torch.tensor([(1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0)], device=device)
+    # image = gsplat2d.rasterize(scale, rotation, means, colors)
+
+    model = gsplat2d.GSplat(15, 15).to(device)
+    model = gsplat2d.GSplat(100).to(device)
+    image = model()
+
+    fig, ax = plt.subplots(1, 1)
+    ax.imshow(image.numpy(force=True))
+    ax.axis("off")
+    fig.tight_layout()
+    fig.savefig(resdir + "raster.png", dpi=300)
+
+    return
+
+def download_config(outdir, resdir):
+    url1 = 'https://raw.githubusercontent.com/OutofAi/2D-Gaussian-Splatting/main/Image-01.png'
+    filename1 = resdir + url1.split('/')[-1]
+    response1 = requests.get(url1)
+    with open(filename1, 'wb') as f:
+        f.write(response1.content)
+
+    url2 = 'https://raw.githubusercontent.com/OutofAi/2D-Gaussian-Splatting/main/config.yml'
+    filename2 = resdir + url2.split('/')[-1]
+    response2 = requests.get(url2)
+    with open(filename2, 'wb') as f:
+        f.write(response2.content)
+    return filename1, filename2
+
+def train(device, outdir, resdir):
+
+    imagefile  = resdir + "Image-01.png"
+    configfile = resdir + "config.yaml"
+
+    # load config
+    # with open(configfile, 'r') as config_file:
+    #     config = yaml.safe_load(config_file)
+    
+    # Extract values from the loaded config
+    config = {
+        "KERNEL_SIZE"            : 101,           # config["KERNEL_SIZE"]
+        "image_size"             : (128, 128, 3), # (256, 256, 3), # tuple(config["image_size"])
+        "primary_gaussians"      : 100, # 1000,          # config["primary_samples"]
+        "backup_gaussians"       : 100, # 4000,          # config["backup_samples"]
+        "num_epochs"             : 2000,          # config["num_epochs"]
+        "densification_interval" : 300,           # config["densification_interval"]
+        "learning_rate"          : 0.001,         # config["learning_rate"]
+        "display_interval"       : 100,           # config["display_interval"]
+        "gradient_threshold"     : 0.002,         # config["gradient_threshold"]
+        "gauss_threshold"        : 0.75,          # config["gaussian_threshold"]
+        "display_loss"           : False,         # config["display_loss"]
+    }
+
+    nG = config["primary_gaussians"]
+    NG = nG + config["backup_gaussians"]
+    image_size = config["image_size"]
+
+    num_epochs = config["num_epochs"]
+    learning_rate = config["learning_rate"]
+    densification_interval = config["densification_interval"]
+    gradient_threshold = config["gradient_threshold"]
+    gauss_threshold = config["gauss_threshold"]
+
+    # get target image
+    target = PIL.Image.open(imagefile)
+    target = target.resize(config["image_size"][0:-1])
+    target = np.array(target.convert("RGB")) / 255.0
+    target = torch.tensor(target).to(torch.float)
+
+    # initialize GSplat
+    model = gsplat2d.GSplat(NG, nG, image_size)
+
+    # move to device
+    model  = model.to(device)
+    target = target.to(device)
+
+    loss_hist = []
+    opt = optim.Adam(model.parameters(), lr=learning_rate)
+
+    for epoch in range(config["num_epochs"]):
+
+        # pruning
+        if epoch % (densification_interval+1) == 0 and epoch > 0:
+            with torch.no_grad():
+                # update model.mask
+                pass
+        #
+
+        opt.zero_grad()
+        image = model()
+        loss  = gsplat2d.combined_loss(image, target, lambda_param=0.2)
+        loss.backward()
+        opt.step()
+
+        print(f"Epoch [{epoch} / {num_epochs}]: LOSS: {loss.item()}")
+
+        # # GC every so often (?)
+        # if epoch % 10 == 0:
+        #     gc.collect()
+        #     torch.cuda.empty_cache()
+    # endfor
+
+    image = model()
+    fig, ax = plt.subplots(1, 1)
+    ax.imshow(image.numpy(force=True))
+    ax.axis("off")
+    fig.tight_layout()
+    fig.savefig(resdir + "out.png", dpi=300)
+
+    return
+
+if __name__ == "__main__":
+    mlutils.set_seed(123)
+
+    parser = argparse.ArgumentParser(description = 'GSplat2D')
+    parser.add_argument('--gpu_device', default=0, help='GPU device', type=int)
+    args = parser.parse_args()
+
+    device = mlutils.select_device()
+    if device == "cuda":
+        device += f":{args.gpu_device}"
+
+    print(f"using device {device}")
+
+    outdir = "./out-gsplat/"
+    resdir = "./res-gsplat/"
+
+    # test_raster(device, outdir, resdir)
+    # imagefile, configfile = download_config(outdir, resdir)
+    train(device, outdir, resdir)
+
+    pass
+#
---
+++ gsplat2d/models.py
@@ -0,0 +1,113 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+# local
+import mlutils
+
+__all__ = [
+    "GSplat",
+    "rasterize",
+]
+
+class GSplat(nn.Module):
+    def __init__(self, N, n=None, image_size=[256, 256, 3]):
+        super().__init__()
+        self.num_gaussans = N
+        if n is None:
+            n = N
+        H, W, C = image_size
+
+        self.image_size = image_size
+        self.scale = nn.Parameter(torch.logit(torch.rand(N, 2)))
+        self.rot   = nn.Parameter(torch.atanh(2 * torch.rand(N, 1) - 1))
+        self.mean  = nn.Parameter(torch.rand(N, 2) * 2 - 1)
+        self.color = nn.Parameter(torch.rand(N, C))
+        self.alpha = nn.Parameter(torch.logit(torch.rand(N, 1)))
+
+        self.register_buffer("mask", torch.ones(N, dtype=torch.bool))
+        self.mask[n:] = 0
+
+    def forward(self):
+        mask  = self.mask.view(-1, 1)
+        scale = torch.sigmoid(self.scale)
+        rot   = torch.tanh(self.rot) * torch.pi / 2
+        mean  = torch.tanh(self.mean)
+        alpha = torch.sigmoid(self.alpha)
+        color = torch.sigmoid(self.color) * alpha.view(-1, 1) * mask
+
+        return rasterize(scale, rot, mean, color, self.image_size)
+#
+
+def rasterize(
+    scale,    # [N, 2] -- COVARIANCE SCALE
+    rot,      # [N, 1] -- COVARIANCE ROTATION ([-pi/2, pi/2])
+    mean,     # [N, 2] -- GAUSSIAN LOCATIONS
+    color,    # [N, C] -- COLORS
+    image_size=[256, 256, 3],
+):
+    H, W, C = image_size
+    N = color.shape[0]
+    rot = rot.view(-1)
+    device = scale.device
+
+    assert rot.shape[0] == scale.shape[0] == color.shape[0] # num Gaussians
+    assert scale.shape[1] == mean.shape[1] == 2               # Dimension == 2
+    assert color.shape[1] == C
+
+    #================#
+    # COVARIANCE MATRICES
+    #================#
+    cos_rot = torch.cos(rot) # [N]
+    sin_rot = torch.sin(rot)
+
+    S = torch.diag_embed(scale)                   # [N, 2, 2] (diagonal)
+    R = torch.stack([                             # [N, 2, 2]
+        torch.stack([cos_rot, -sin_rot], dim=-1), # [N, 2]
+        torch.stack([sin_rot,  cos_rot], dim=-1), # [N, 2]
+    ], dim=-2)
+
+    COV = R @ S @ S @ R.transpose(-1, -2) # [N, 2, 2]
+    CINV = torch.inverse(COV)
+
+    #================#
+    # CREATE GRID CORRESPONDING TO TARGET IMAGE
+    #================#
+    L = 5.0
+    x = torch.linspace(-L, L, H, device=device)
+    y = torch.linspace(-L, L, W, device=device)
+    xx, yy = torch.meshgrid(x, y, indexing="ij")
+    xy = torch.stack([xx, yy], dim=-1)         # [H, W, 2]
+    xy = xy.unsqueeze(0).expand(N, -1, -1, -1) # [N, H, W, 2]
+
+    #================#
+    # FORM GAUSSIAN KERNEL
+    #================#
+    z = torch.einsum('nxyi, nij, nxyj -> nxy', xy, -0.5 * CINV, xy) # [N, H, W]
+    kernel = torch.exp(z) / (2 * torch.pi * torch.sqrt(torch.det(COV))).view(N, 1, 1)
+
+    # normalize kernel
+    kernel = kernel / kernel.amax(dim=(-2, -1), keepdim=True) # [N, H, W]
+
+    # add color channels
+    kernel = kernel.unsqueeze(1).expand(-1, C, -1, -1) # [N, C, H, W]
+
+    # translate grid
+    theta = torch.zeros(N, 2, 3, device=device)
+    theta[:, 0, 0] = 1.0
+    theta[:, 1, 1] = 1.0
+    theta[:, :, 2] = mean
+
+    grid = F.affine_grid(theta, size=(N, C, H, W), align_corners=True)
+    kernel = F.grid_sample(kernel, grid, align_corners=True)
+
+    #================#
+    # IMAGE
+    #================#
+    image = kernel * color.unsqueeze(-1).unsqueeze(-1)
+    image = image.sum(dim=0)         # sum over Gaussians
+    image = torch.clamp(image, 0, 1) # clamp values to [0, 1]
+    image = image.permute(1, 2, 0)   # permute to [H, W, C]
+
+    return image
+#
