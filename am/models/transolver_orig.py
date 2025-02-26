d183555036241dd8f144fb8f71a122f6de24c3c1
Vedant Puri <vedantpuri@gmail.com>
2025-02-26 18:54:31 -0500

    transolver original code

---
+++ am/models/transolver_orig.py
@@ -0,0 +1,257 @@
+#
+# https://github.com/thuml/Transolver/blob/main/Car-Design-ShapeNetCar/models/Transolver.py
+import torch
+from torch import nn
+from torch.nn import functional as F
+from timm.layers import trunc_normal_
+from einops import rearrange, repeat
+
+__all__ = [
+    "Transolver",
+    "Transolver_block",
+    "PhysicsAttention",
+]
+
+ACTIVATION = {'gelu': nn.GELU, 'tanh': nn.Tanh, 'sigmoid': nn.Sigmoid, 'relu': nn.ReLU, 'leaky_relu': nn.LeakyReLU(0.1),
+              'softplus': nn.Softplus, 'ELU': nn.ELU, 'silu': nn.SiLU}
+
+class PhysicsAttention(nn.Module):
+    def __init__(self, dim, heads=8, dim_head=64, dropout=0., slice_num=64):
+        super().__init__()
+        inner_dim = dim_head * heads
+        self.dim_head = dim_head
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        self.softmax = nn.Softmax(dim=-1)
+        self.dropout = nn.Dropout(dropout)
+        self.temperature = nn.Parameter(torch.ones([1, heads, 1, 1]) * 0.5)
+
+        self.in_project_x = nn.Linear(dim, inner_dim)
+        self.in_project_fx = nn.Linear(dim, inner_dim)
+        self.in_project_slice = nn.Linear(dim_head, slice_num)
+        for l in [self.in_project_slice]:
+            torch.nn.init.orthogonal_(l.weight)  # use a principled initialization
+        self.to_q = nn.Linear(dim_head, dim_head, bias=False)
+        self.to_k = nn.Linear(dim_head, dim_head, bias=False)
+        self.to_v = nn.Linear(dim_head, dim_head, bias=False)
+        # self.mha  = nn.MultiheadAttention(dim, heads)
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        B, N, C = x.shape
+
+        ### (1) Slice
+        # VALUE (B H N C)
+        fx_mid = self.in_project_fx(x).reshape(B, N, self.heads, self.dim_head).permute(0, 2, 1, 3).contiguous()
+        # KEY (B H N C)
+        x_mid = self.in_project_x(x).reshape(B, N, self.heads, self.dim_head).permute(0, 2, 1, 3).contiguous()
+
+        # QUERY = weights of self.in_project_slice (dim_head, slice_num)
+        #
+        # ATTN_WEIGHTS = SLICE_WEIGHTS = softmax(K * Q)
+        # SOFTMAX is happening along (-1) query direction.
+        # PERCEIVER IO (https://arxiv.org/pdf/2107.14795) did it in (-2) direction
+        
+        slice_weights = self.softmax(self.in_project_slice(x_mid) / self.temperature)  # B H N G
+        slice_norm = slice_weights.sum(2)  # B H G
+        # V * ATTN_WEIGHTS
+        slice_token = torch.einsum("bhnc,bhng->bhgc", fx_mid, slice_weights)
+        slice_token = slice_token / ((slice_norm + 1e-5)[:, :, :, None].repeat(1, 1, 1, self.dim_head))
+
+        # IDEAS:
+        # - Transolver++: Gimble softmax with temperature = 1 + self.temperature_project(x) (Transolver)
+        # - Transolver++: remove fx_mid and use x_mid for both key and value
+        # - Make query dependent on x? Is that possible? Would it have the right dimension?
+        # - How does this constrast with attention encodings? See Latent diffusion transformer for point cloud generation paper.
+        # - Pass in X/Y/Z coordinates to every transolver block and concatenate to the input.
+
+        # - AdaLayerNorm (DiT, https://arxiv.org/abs/2302.07459) like conditioning on t, dt, process paramters
+        # - can we accomplish bulk masking with the conditioning idea?
+        # - that is make conditioning focus more on top layers
+        # - solution: allow the slice weights to be conditioned on t, dt
+        
+        # - mess with training schedule: OneCycleLR(max_Lr=1e-1, pct_start=0.1), weight_decay=0
+        # - Do ReZero (https://proceedings.mlr.press/v161/bachlechner21a/bachlechner21a.pdf)
+        #   in Transolver_block
+
+        ### (2) Attention among slice tokens
+        q_slice_token = self.to_q(slice_token)
+        k_slice_token = self.to_k(slice_token)
+        v_slice_token = self.to_v(slice_token)
+        dots = torch.matmul(q_slice_token, k_slice_token.transpose(-1, -2)) * self.scale
+        attn = self.softmax(dots)
+        attn = self.dropout(attn)
+        out_slice_token = torch.matmul(attn, v_slice_token)  # B H G D
+
+        # out_slice_token = F.scaled_dot_product_attention( # [B H, G, D]
+        #     q_slice_token, k_slice_token, v_slice_token,
+        #     # dropout_p=self.dropout,
+        #     # scale=self.scale,
+        # )
+
+        ### (3) Deslice
+        out_x = torch.einsum("bhgc,bhng->bhnc", out_slice_token, slice_weights)
+        out_x = rearrange(out_x, 'b h n d -> b n (h d)')
+
+        return self.to_out(out_x)
+
+
+class MLP(nn.Module):
+    def __init__(self, n_input, n_hidden, n_output,
+                 n_layers=1, act='gelu', res=True):
+        super(MLP, self).__init__()
+
+        if act in ACTIVATION.keys():
+            act = ACTIVATION[act]
+        else:
+            raise NotImplementedError
+        self.n_input  = n_input
+        self.n_hidden = n_hidden
+        self.n_output = n_output
+        self.n_layers = n_layers
+        self.res = res
+        self.linear_pre = nn.Sequential(nn.Linear(n_input, n_hidden), act())
+        self.linear_post = nn.Linear(n_hidden, n_output)
+        self.linears = nn.ModuleList([nn.Sequential(nn.Linear(n_hidden, n_hidden), act()) for _ in range(n_layers)])
+
+    def forward(self, x):
+        x = self.linear_pre(x)
+        for i in range(self.n_layers):
+            if self.res:
+                x = self.linears[i](x) + x
+            else:
+                x = self.linears[i](x)
+        x = self.linear_post(x)
+        return x
+
+
+class Transolver_block(nn.Module):
+    """Transformer encoder block."""
+
+    def __init__(
+            self,
+            num_heads: int,
+            hidden_dim: int,
+            dropout: float,
+            act='gelu',
+            mlp_ratio=4,
+            last_layer=False,
+            out_dim=1,
+            slice_num=32,
+    ):
+        super().__init__()
+        self.last_layer = last_layer
+        self.ln_1 = nn.LayerNorm(hidden_dim)
+        self.Attn = PhysicsAttention(hidden_dim, heads=num_heads, dim_head=hidden_dim // num_heads,
+                                     dropout=dropout, slice_num=slice_num)
+        self.ln_2 = nn.LayerNorm(hidden_dim)
+        self.mlp = MLP(hidden_dim, int(hidden_dim * mlp_ratio), hidden_dim, n_layers=0, res=False, act=act)
+        if self.last_layer:
+            self.ln_3 = nn.LayerNorm(hidden_dim)
+            self.mlp2 = nn.Linear(hidden_dim, out_dim)
+
+    def forward(self, fx):
+        fx = self.Attn(self.ln_1(fx)) + fx
+        fx = self.mlp(self.ln_2(fx)) + fx
+        if self.last_layer:
+            return self.mlp2(self.ln_3(fx))
+        else:
+            return fx
+
+ # Transolver(
+ #   n_hidden=256,
+ #   n_layers=8,
+ #   space_dim=7,
+ #   fun_dim=0,
+ #   out_dim=4,
+ #   mlp_ratio=2,
+ #   n_head=8,
+ #   slice_num=32,
+ # )
+
+class Transolver(nn.Module):
+    def __init__(self,
+                 space_dim=1,
+                 n_layers=5,
+                 n_hidden=256,
+                 dropout=0,
+                 n_head=8,
+                 act='gelu',
+                 mlp_ratio=1,
+                 fun_dim=1,
+                 out_dim=1,
+                 slice_num=32,
+                 ):
+        super(Transolver, self).__init__()
+        self.__name__ = 'Transolver'
+        self.preprocess = MLP(fun_dim + space_dim, n_hidden * 2, n_hidden,
+                              n_layers=0, res=False, act=act)
+        self.n_hidden = n_hidden
+        self.space_dim = space_dim
+
+        self.blocks = nn.ModuleList([
+            Transolver_block(num_heads=n_head, hidden_dim=n_hidden,
+                             dropout=dropout,
+                             act=act,
+                             mlp_ratio=mlp_ratio,
+                             out_dim=out_dim,
+                             slice_num=slice_num,
+                             last_layer=(_ == n_layers - 1))
+            for _ in range(n_layers)
+        ])
+        self.initialize_weights()
+        self.placeholder = nn.Parameter((1 / (n_hidden)) * torch.rand(n_hidden, dtype=torch.float))
+
+    def initialize_weights(self):
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=0.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, (nn.LayerNorm, nn.BatchNorm1d)):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    def forward(self, data):
+        x = data.x.unsqueeze(0)                    # space dim [B, N, C]
+        f = data.f if hasattr(data, 'f') else None # func  dim [B, N, C]
+
+        if f is not None:
+            f = torch.cat((x, f), -1)
+            f = self.preprocess(f)
+        else:
+            f = self.preprocess(x)
+            f = f + self.placeholder[None, None, :]
+
+        for block in self.blocks:
+            f = block(f)
+
+        return f[0]
+
+    # def forward(self, data):
+    #     x = data.x
+    #     f = data.f if hasattr(data, 'f') else None # func  dim
+    #     t = data.t if hasattr(data, 't') else None
+    #
+    #     if f is not None:
+    #         f = torch.cat((x, f), -1)
+    #         f = self.preprocess(f)
+    #     else:
+    #         f = self.preprocess(x)
+    #         f = f + self.placeholder[None, None, :]
+    #
+    #     if t is not None:
+    #         t = timestep_embedding(t, self.n_hidden).repeat(1, x.shape[1], 1)
+    #         t = self.time_fc(t)
+    #         f = f + t
+    #
+    #     for block in self.blocks:
+    #         f = block(f)
+    #
+    #     return f
+#
