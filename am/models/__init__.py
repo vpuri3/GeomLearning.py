b589a1dc6714e4672441aae8856fadaa5a4bc0de
Vedant Puri <vedantpuri@gmail.com>
2025-03-02 01:09:25 -0500

    add comments to am.models.__init__

--- am/models/__init__.py
+++ am/models/__init__.py
@@ -1,10 +1,10 @@
 from .masked import *
 from .meshGNN import *
-from .transolver import *
 
-from .ts import *
-from .ts1 import *
-from .ts2 import *
-from .ts3 import *
-from .ts4 import *
+from .transolver import * # OG Transolver
+from .ts  import * # Slice attention + no conditioning
+from .ts1 import * # Physics attention + AdaLN conditioning
+from .ts2 import * # Slice attention + AdaLN conditioning
+from .ts3 import * # Slice attention (+ temperature) + AdaLN conditioning
+from .ts4 import * # Slice attention (+ temperature) + slice query conditioning
 #
