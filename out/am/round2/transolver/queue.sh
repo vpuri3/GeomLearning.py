6aa042ee6c7ab19f33eb546cab29256838bc8ef9
Vedant Puri <vedantpuri@gmail.com>
2025-03-01 22:05:08 -0500

    reorganize round2

--- out/am/round2/transolver/queue.sh
+++ out/am/round2/transolver/queue.sh
@@ -1,24 +1,24 @@
 #
 
-EXP_NAME="ts0"
+EXP_NAME="ts0" # no conv. ckpt10 -> 1.12e-4
 torchrun --nproc_per_node gpu -m am --exp_name ${EXP_NAME} --train true \
     --epochs 200 --timeseries true --sdf true --TRA true --tra_width 128 \
     --tra_num_heads 8 --tra_num_slices 32 --tra_num_layers 8 \
     --weight_decay 1e-4 --learning_rate 1e-4
 
-EXP_NAME="ts1"
+EXP_NAME="ts1" # cts improvement. ckpt10 -> 8.7e-5
 torchrun --nproc_per_node gpu -m am --exp_name ${EXP_NAME} --train true \
     --epochs 200 --timeseries true --sdf true --TRA true --tra_width 128 \
     --tra_num_heads 8 --tra_num_slices 32 --tra_num_layers 8 \
     --weight_decay 1e-2 --learning_rate 1e-4
 
-EXP_NAME="ts2"
+EXP_NAME="ts2" # no conv. ckpt10 -> 1.18e-4
 torchrun --nproc_per_node gpu -m am --exp_name ${EXP_NAME} --train true \
     --epochs 200 --timeseries true --sdf true --TRA true --tra_width 128 \
     --tra_num_heads 8 --tra_num_slices 32 --tra_num_layers 8 \
     --weight_decay 1e-1 --learning_rate 1e-4
 
-EXP_NAME="ts3"
+EXP_NAME="ts3" # conv at ckpt09/10. ckpt10 -> 7.8e-5
 torchrun --nproc_per_node gpu -m am --exp_name ${EXP_NAME} --train true \
     --epochs 200 --timeseries true --sdf true --TRA true --tra_width 128 \
     --tra_num_heads 8 --tra_num_slices 32 --tra_num_layers 8 \
@@ -26,25 +26,25 @@ torchrun --nproc_per_node gpu -m am --exp_name ${EXP_NAME} --train true \
 
 #---------------------------#
 
-EXP_NAME="ts4"
+EXP_NAME="ts4" # conv at ckpt06 -> 7.41e-5. cts improvement to ckpt10 5.52e-5
 torchrun --nproc_per_node gpu -m am --exp_name ${EXP_NAME} --train true \
     --epochs 200 --timeseries true --sdf true --TRA true --tra_width 128 \
     --tra_num_heads 8 --tra_num_slices 32 --tra_num_layers 8 \
     --schedule OneCycleLR --learning_rate 1e-3 --weight_decay 0e-3
 
-EXP_NAME="ts5"
+EXP_NAME="ts5" # conv at ckpt06 -> 7.39e-5. cts improvement to ckpt10 5.32e-5
 torchrun --nproc_per_node gpu -m am --exp_name ${EXP_NAME} --train true \
     --epochs 200 --timeseries true --sdf true --TRA true --tra_width 128 \
     --tra_num_heads 8 --tra_num_slices 32 --tra_num_layers 8 \
     --schedule OneCycleLR --learning_rate 1e-3 --weight_decay 1e-3
 
-EXP_NAME="ts6"
+EXP_NAME="ts6" # no conv. ckpt10 -> 1.04e-4
 torchrun --nproc_per_node gpu -m am --exp_name ${EXP_NAME} --train true \
     --epochs 200 --timeseries true --sdf true --TRA true --tra_width 128 \
     --tra_num_heads 8 --tra_num_slices 32 --tra_num_layers 8 \
     --schedule OneCycleLR --learning_rate 1e-3 --weight_decay 1e-2
 
-EXP_NAME="ts7"
+EXP_NAME="ts7" # cts improvement. ckpt10 -> 7.38e-5
 torchrun --nproc_per_node gpu -m am --exp_name ${EXP_NAME} --train true \
     --epochs 200 --timeseries true --sdf true --TRA true --tra_width 128 \
     --tra_num_heads 8 --tra_num_slices 32 --tra_num_layers 8 \
