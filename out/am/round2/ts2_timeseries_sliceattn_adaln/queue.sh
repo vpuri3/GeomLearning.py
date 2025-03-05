467d587a6aa4d81af86ae3e90f7edc4453b85da8
Vedant Puri <vedantpuri@gmail.com>
2025-03-05 18:08:05 -0500

    TS2 - slice attn with wt_q [M,D] and adaln - mostly done

---
+++ out/am/round2/ts2_timeseries_sliceattn_adaln/queue.sh
@@ -0,0 +1,61 @@
+#
+
+EXP_NAME="ts0" # cts improvement ckpt10 -> 6.14-5
+torchrun --master_port 29501 \
+    --nproc_per_node gpu -m am --exp_name ${EXP_NAME} --train true \
+    --epochs 200 --timeseries true --sdf true --TRA 2 --tra_width 128 \
+    --tra_num_heads 8 --tra_num_slices 32 --tra_num_layers 8 \
+    --weight_decay 1e-4 --learning_rate 1e-4
+
+EXP_NAME="ts1" # ckptp10 -> 6.83e-5
+torchrun --master_port 29501 \
+    --nproc_per_node gpu -m am --exp_name ${EXP_NAME} --train true \
+    --epochs 200 --timeseries true --sdf true --TRA 2 --tra_width 128 \
+    --tra_num_heads 8 --tra_num_slices 32 --tra_num_layers 8 \
+    --weight_decay 1e-3 --learning_rate 1e-4
+
+EXP_NAME="ts2" # ckpt10 -> 7.89e-5
+torchrun --master_port 29501 \
+    --nproc_per_node gpu -m am --exp_name ${EXP_NAME} --train true \
+    --epochs 200 --timeseries true --sdf true --TRA 2 --tra_width 128 \
+    --tra_num_heads 8 --tra_num_slices 32 --tra_num_layers 8 \
+    --weight_decay 1e-2 --learning_rate 1e-4
+
+EXP_NAME="ts3" # ckpt08 -> 8.32e-5, ckpt10 -> 9.61e-5
+torchrun --master_port 29501 \
+    --nproc_per_node gpu -m am --exp_name ${EXP_NAME} --train true \
+    --epochs 200 --timeseries true --sdf true --TRA 2 --tra_width 128 \
+    --tra_num_heads 8 --tra_num_slices 32 --tra_num_layers 8 \
+    --weight_decay 1e-1 --learning_rate 1e-4
+
+#---------------------------#
+
+EXP_NAME="ts4" # ckpt10 -> 8.29e-5
+torchrun --master_port 29501 \
+    --nproc_per_node gpu -m am --exp_name ${EXP_NAME} --train true \
+    --epochs 200 --timeseries true --sdf true --TRA 2 --tra_width 128 \
+    --tra_num_heads 8 --tra_num_slices 32 --tra_num_layers 8 \
+    --schedule OneCycleLR --learning_rate 1e-3 --weight_decay 1e-4
+
+EXP_NAME="ts5" # ckpt10 -> 2.00e-4
+torchrun --master_port 29501 \
+    --nproc_per_node gpu -m am --exp_name ${EXP_NAME} --train true \
+    --epochs 200 --timeseries true --sdf true --TRA 2 --tra_width 128 \
+    --tra_num_heads 8 --tra_num_slices 32 --tra_num_layers 8 \
+    --schedule OneCycleLR --learning_rate 1e-3 --weight_decay 1e-3
+
+EXP_NAME="ts6" #
+torchrun --master_port 29501 \
+    --nproc_per_node gpu -m am --exp_name ${EXP_NAME} --train true \
+    --epochs 200 --timeseries true --sdf true --TRA 2 --tra_width 128 \
+    --tra_num_heads 8 --tra_num_slices 32 --tra_num_layers 8 \
+    --schedule OneCycleLR --learning_rate 1e-3 --weight_decay 1e-2
+
+EXP_NAME="ts7" # 
+torchrun --master_port 29501 \
+    --nproc_per_node gpu -m am --exp_name ${EXP_NAME} --train true \
+    --epochs 200 --timeseries true --sdf true --TRA 2 --tra_width 128 \
+    --tra_num_heads 8 --tra_num_slices 32 --tra_num_layers 8 \
+    --schedule OneCycleLR --learning_rate 1e-3 --weight_decay 1e-1
+
+#
