d31bf9c83f2fdca7a8c0caf9ac55c2e87defb04d
Vedant Puri <vedantpuri@gmail.com>
2025-04-01 16:54:22 -0400

    move callback to mlutils

--- bench/callbacks.py
+++ bench/callbacks.py
@@ -20,114 +20,12 @@ from am.callbacks import timeseries_statistics_plot
 from am.callbacks import hstack_dataframes_across_ranks, vstack_dataframes_across_ranks
 
 __all__ = [
-    'Callback',
     'TimeseriesCallback',
     'TSCallback',
 ]
 
 #======================================================================#
-class Callback:
-    def __init__(self, case_dir: str, save_every=None):
-        self.case_dir = case_dir
-        self.save_every = save_every
-        self.final = False
-
-    def get_ckpt_dir(self, trainer: mlutils.Trainer):
-        if self.final:
-            ckpt_dir = os.path.join(self.case_dir, f'final')
-        else:
-            nsave = trainer.epoch // self.save_every
-            ckpt_dir = os.path.join(self.case_dir, f'ckpt{str(nsave).zfill(2)}')
-
-        if os.path.exists(ckpt_dir) and trainer.GLOBAL_RANK == 0:
-            print(f"Removing {ckpt_dir}")
-            shutil.rmtree(ckpt_dir)
-
-        return ckpt_dir
-
-    def load(self, trainer: mlutils.Trainer):
-        ckpt_dirs = [dir for dir in os.listdir(self.case_dir) if dir.startswith('ckpt')]
-        if len(ckpt_dirs) == 0:
-            if trainer.GLOBAL_RANK == 0:
-                print(f'No checkpoint found in {self.case_dir}. starting from scrach.')
-            return
-        load_dir = sorted(ckpt_dirs)[-1]
-        model_file = os.path.join(self.case_dir, load_dir, 'model.pt')
-
-        trainer.load(model_file)
-
-        return
-
-    @torch.no_grad()
-    def __call__(self, trainer: mlutils.Trainer, final: bool=False):
-
-        #------------------------#
-        self.final = final
-        if not self.final:
-            if self.save_every is None:
-                self.save_every = trainer.stats_every
-            if trainer.epoch == 0:
-                # return
-                pass
-            if (trainer.epoch % self.save_every) != 0:
-                return
-        #------------------------#
-
-        # save model
-        ckpt_dir = self.get_ckpt_dir(trainer)
-        if trainer.GLOBAL_RANK == 0:
-            print(f"saving checkpoint to {ckpt_dir}")
-            os.makedirs(ckpt_dir, exist_ok=True)
-            trainer.save(os.path.join(ckpt_dir, 'model.pt'))
-
-        # save stats
-        if trainer.GLOBAL_RANK == 0:
-            print(f"Saving stats to {ckpt_dir}/stats.json")
-            with open(os.path.join(ckpt_dir, 'stats.json'), 'w') as f:
-                json.dump(trainer.stat_vals, f)
-
-        # save training loss plot
-        if trainer.GLOBAL_RANK == 0:
-            print(f"Saving loss plot to {self.case_dir}/training_loss.png")
-            plt.figure(figsize=(8, 4), dpi=175)
-            training_loss = trainer.training_loss
-            if isinstance(training_loss, list):
-                training_loss = torch.tensor(training_loss)
-            training_loss[training_loss < 1e-12] = torch.nan
-            plt.plot(training_loss, color='k', label='Training Loss')
-            plt.xlabel('Step')
-            plt.ylabel('Loss')
-            plt.yscale('log')
-            if trainer.stat_vals['train_loss'] is not None:
-                plt.title('Training Loss, final: {:.2e}'.format(trainer.stat_vals['train_loss']))
-            else:
-                plt.title('Training Loss')
-            plt.legend()
-            plt.tight_layout()
-            plt.savefig(os.path.join(self.case_dir, 'training_loss.png'))
-            plt.close()
-
-        # modify dataset transform
-        if hasattr(self, 'modify_dataset_transform'):
-            self.modify_dataset_transform(trainer, True)
-
-        # evaluate model
-        self.evaluate(trainer, ckpt_dir)
-
-        # revert dataset transform
-        if hasattr(self, 'modify_dataset_transform'):
-            self.modify_dataset_transform(trainer, False)
-
-        # revert self.final
-        self.final = False
-
-        return
-    
-    def evaluate(self, trainer: mlutils.Trainer, ckpt_dir: str):
-        pass
-
-#======================================================================#
-class TimeseriesCallback(Callback):
+class TimeseriesCallback(mlutils.Callback):
     def __init__(
         self,
         case_dir: str, save_every=None,
@@ -261,7 +159,7 @@ class TimeseriesCallback(Callback):
 
 
 #======================================================================#
-class TSCallback(Callback):
+class TSCallback(mlutils.Callback):
     # def __init__(self, case_dir: str, save_every=None):
     #     super().__init__(case_dir, save_every)
 
